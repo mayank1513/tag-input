1ebcd2f9b55b0e13ecc23a5f1cc76168daaa4ed1
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-09-17 12:02:06 +0530

    Autocomlete with API example

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Features from "./pages/Features.vue";
 import GettingStarted from "./pages/GettingStarted.vue";
 import Autocomplete from "./pages/examples/Autocomplete.vue";
+import AutocompleteWithAPI from "./pages/examples/AutocompleteWithAPI.vue";
 import CustomDelimeter from "./pages/examples/CustomDelimeter.vue";
 import CustomPlaceholder from "./pages/examples/CustomPlaceholder.vue";
 
@@ -9,6 +10,7 @@ const routes: RouteRecordRaw[] = [
   { path: "/", component: Features },
   { path: "/getting-started", component: GettingStarted },
   { path: "/examples/autocomplete", component: Autocomplete },
+  { path: "/examples/autocomplete-with-api", component: AutocompleteWithAPI },
   { path: "/examples/custom-delimeter", component: CustomDelimeter },
   { path: "/examples/custom-placeholder", component: CustomPlaceholder },
 ];
