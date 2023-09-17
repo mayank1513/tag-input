6a9ac1eb8a3eebee13a943c0a1f499d3eeb30c5f
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-09-17 12:49:50 +0530

    fix: custom validation message add docs for validation

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Features from "./pages/Features.vue";
 import GettingStarted from "./pages/GettingStarted.vue";
 import Autocomplete from "./pages/examples/Autocomplete.vue";
 import AutocompleteWithAPI from "./pages/examples/AutocompleteWithAPI.vue";
+import Validation from "./pages/examples/Validation.vue";
 import CustomDelimeter from "./pages/examples/CustomDelimeter.vue";
 import CustomPlaceholder from "./pages/examples/CustomPlaceholder.vue";
 
@@ -11,6 +12,7 @@ const routes: RouteRecordRaw[] = [
   { path: "/getting-started", component: GettingStarted },
   { path: "/examples/autocomplete", component: Autocomplete },
   { path: "/examples/autocomplete-with-api", component: AutocompleteWithAPI },
+  { path: "/examples/validation", component: Validation },
   { path: "/examples/custom-delimeter", component: CustomDelimeter },
   { path: "/examples/custom-placeholder", component: CustomPlaceholder },
 ];
